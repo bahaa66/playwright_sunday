03cc542c583a74ac0c5a956783c7c944a0f6ab88
amacciola <amacciola@cogility.com>
2020-02-14 11:34:54 -0800

    Stop Broadway Pipelines
    update RPC call to shutdown broadway pipelines

--- lib/cogynt_workstation_ingest_web/rpc/ingest_handler.ex
+++ lib/cogynt_workstation_ingest_web/rpc/ingest_handler.ex
@@ -1,7 +1,13 @@
 defmodule CogyntWorkstationIngestWeb.Rpc.IngestHandler do
   use JSONRPC2.Server.Handler
 
-  alias CogyntWorkstationIngest.Supervisors.ConsumerGroupSupervisor
+  alias CogyntWorkstationIngest.Supervisors.{
+    ConsumerGroupSupervisor,
+    EventSupervisor,
+    LinkEventSupervisor
+  }
+
+  @linkage Application.get_env(:cogynt_workstation_ingest, :core_keys)[:link_data_type]
 
   def handle_request("start:consumer", event_definition) when is_map(event_definition) do
     result = ConsumerGroupSupervisor.start_child(keys_to_atoms(event_definition))
@@ -29,22 +35,34 @@ defmodule CogyntWorkstationIngestWeb.Rpc.IngestHandler do
 
   def handle_request("stop:consumer", event_definition) when is_map(event_definition) do
     event_definition = keys_to_atoms(event_definition)
-    result = ConsumerGroupSupervisor.stop_child(event_definition.topic)
 
-    case result do
+    with :ok <- ConsumerGroupSupervisor.stop_child(event_definition.topic),
+         :ok <- EventSupervisor.stop_child(event_definition.topic),
+         true <- link_event?(event_definition),
+         :ok <- LinkEventSupervisor.stop_child(event_definition.topic) do
+      %{
+        status: :ok,
+        body: :success
+      }
+    else
+      false ->
+        %{
+          status: :ok,
+          body: :success
+        }
+
       {:error, error} ->
         %{
           status: :error,
           body: "#{inspect(error)}"
         }
-
-      :ok ->
-        %{
-          status: :ok,
-          body: :success
-        }
     end
   end
+
+  # ----------------------- #
+  # --- private methods --- #
+  # ----------------------- #
+  defp link_event?(%{event_type: type}), do: type == @linkage
 
   defp keys_to_atoms(string_key_map) do
     for {key, val} <- string_key_map, into: %{}, do: {String.to_atom(key), val}
