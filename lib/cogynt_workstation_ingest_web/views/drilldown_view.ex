f3d7a6b6aaa4d3e98c3dabe4a7023c9279c7ee57
amacciola <amacciola@cogility.com>
2020-03-06 09:57:35 -0800

    Adding improvements to Drilldown
    Addresses 3 issues in drill down:
    
    1) It could hang with circular chains of events.  It now tracks all events previously included in the output and prunes the tree to remove duplicates.
    
    2) It could show multiple versions of the same event, it now properly updates the event with newer versions.
    
    3) It would show risk events/instances that did not fully form due to missing partition keys.  It currently filters out partial events with confidence 0 (assuming that if risk is enabled it will produce some number over 0 if any meaningful events are present).  This assumption can be revisited, but appears to clean up the diagram significantly.

--- lib/cogynt_workstation_ingest_web/views/drilldown_view.ex
+++ lib/cogynt_workstation_ingest_web/views/drilldown_view.ex
@@ -1,21 +1,23 @@
 defmodule CogyntWorkstationIngestWeb.DrilldownView do
   use CogyntWorkstationIngestWeb, :view
   use JaSerializer.PhoenixView
+  import Logger, warn: false
   alias CogyntWorkstationIngestWeb.JA_Keys
   alias CogyntWorkstationIngest.Servers.Caches.DrilldownCache
   alias CogyntWorkstatingIngest.EventView
 
   def type, do: "solutions"
 
-  def id info, _conn do
+  def id(info, _conn) do
     info["id"]
   end
 
-  def attributes info, _conn do
+  def attributes(info, _conn) do
     info
     |> Map.delete("id")
     |> Map.delete(:id)
     |> Map.delete("events")
+    |> Map.delete("#visited")
     |> JA_Keys.dasherize()
   end
 
@@ -24,30 +26,47 @@ defmodule CogyntWorkstationIngestWeb.DrilldownView do
     include: true,
     identifiers: :always
 
-  def children info, _conn do
-    (info["events"] || [])
+  def children(info, _conn) do
+    IO.inspect(info, label: "@@@@ Parent in drilldown")
+
+    (Map.values(info["events"]) || [])
+    |> Enum.filter(&(not (&1["$partial"] == true and &1["_confidence"] == 0.0)))
     |> Enum.map(fn occ ->
       id = occ["published_by"]
+
       if id do
         {:ok, inst} = DrilldownCache.get(id)
-        inst
+        # inst
+        if info["id"] == inst["id"] or Enum.member?(info["#visited"], inst["id"]) do
+          IO.inspect(occ, label: "@@@@ Event published by self?")
+          IO.inspect(inst, label: "@@@@ Instance causing recursion")
+          nil
+        else
+          Logger.debug("@@@@ Instance #{inst["id"]} parent #{info["key"]}")
+          inst
+        end
       else
         nil
       end
     end)
-    |> Enum.filter(& &1 != nil)
-    |> Enum.uniq
+    |> Enum.filter(&(&1 != nil))
+    |> Enum.uniq()
     |> Enum.map(fn inst ->
+      inst_id = inst["id"]
+      visited = [inst_id | info["#visited"]]
       # Convert id to a unique id combining parent and child ids
       # put original id in "key", only if the parent has key.  This
       # ensures drill down has unique ids for all nodes, but index and
       # show use the real id.
       if info["key"] do
-        Map.put(inst, "key", inst["id"])
-        |> Map.put("id", info["id"]<>"|"<>inst["id"])
+        inst
+        |> Map.put("key", inst["id"])
+        |> Map.put("id", info["id"] <> "|" <> inst["id"] <> "|" <> (inst["assertion_id"] || ""))
       else
         inst
       end
+      |> Map.put("#visited", visited)
+      |> IO.inspect(label: "@@@@ child")
     end)
     |> Enum.sort_by(& &1["id"])
   end
@@ -57,8 +76,15 @@ defmodule CogyntWorkstationIngestWeb.DrilldownView do
     include: true,
     identifiers: :always
 
-  def events info, _conn do
-    info["events"]
-    |> Enum.sort_by(& &1["id"])
+  def events(info, _conn) do
+    if is_map(info["events"]) do
+      Map.values(info["events"])
+      |> Enum.filter(&(not (&1["$partial"] == true and &1["_confidence"] == 0.0)))
+      |> Enum.sort_by(& &1["id"])
+    else
+      info["events"]
+      |> Enum.filter(&(not (&1["$partial"] == true and &1["_confidence"] == 0.0)))
+      |> Enum.sort_by(& &1["id"])
+    end
   end
 end
