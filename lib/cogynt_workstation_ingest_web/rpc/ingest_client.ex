722516decc5d4a6728e606a0e65aa0832af49030
amacciola <amacciola@cogility.com>
2020-03-11 11:27:04 -0700

    Rpc routes through phoenix

--- lib/cogynt_workstation_ingest_web/rpc/ingest_client.ex
+++ lib/cogynt_workstation_ingest_web/rpc/ingest_client.ex
@@ -1,10 +1,13 @@
 defmodule CogyntWorkstationIngestWeb.Rpc.IngestClient do
   alias JSONRPC2.Clients.HTTP
 
-  @url Application.get_env(:cogynt_workstation_ingest, :rpc)[:cogynt_otp_url]
+  @domain Application.get_env(:cogynt_workstation_ingest, :rpc)[:cogynt_domain]
+  @port Application.get_env(:cogynt_workstation_ingest, :rpc)[:cogynt_port]
+  @path "/rpc/ingest"
 
   def publish_deleted_notifications(notifications) when is_list(notifications) do
-    response = HTTP.call(@url, "publish:deleted_notifications", notifications)
+    url = "#{@domain}:#{@port}#{@path}"
+    response = HTTP.call(url, "publish:deleted_notifications", notifications)
 
     case response do
       {:ok, %{"body" => body, "status" => status}} when status == "ok" ->
@@ -19,7 +22,8 @@ defmodule CogyntWorkstationIngestWeb.Rpc.IngestClient do
   end
 
   def publish_subscriptions(notifications) when is_list(notifications) do
-    response = HTTP.call(@url, "publish:subscriptions", notifications)
+    url = "#{@domain}:#{@port}#{@path}"
+    response = HTTP.call(url, "publish:subscriptions", notifications)
 
     case response do
       {:ok, %{"body" => body, "status" => status}} when status == "ok" ->
@@ -34,7 +38,8 @@ defmodule CogyntWorkstationIngestWeb.Rpc.IngestClient do
   end
 
   def publish_event_definition_ids(event_definition_ids) when is_list(event_definition_ids) do
-    response = HTTP.call(@url, "publish:event_definition_ids", event_definition_ids)
+    url = "#{@domain}:#{@port}#{@path}"
+    response = HTTP.call(url, "publish:event_definition_ids", event_definition_ids)
 
     case response do
       {:ok, %{"body" => body, "status" => status}} when status == "ok" ->
