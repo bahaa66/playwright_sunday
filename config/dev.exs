fabf294f2025b4a9cde682a085faa56d32bd91d0
langleyl <llangley@cogility.com>
2020-03-13 13:44:47 -0700

    fix(build): config clean up.

--- config/dev.exs
+++ config/dev.exs
@@ -73,6 +73,7 @@ config :cogynt_workstation_ingest, :core_keys,
   delete: System.get_env("CORE_KEYS_DELETE") || "delete",
   create: System.get_env("CORE_KEYS_CREATE") || "create"
 
+# Broadway Pipelines configurations
 config :cogynt_workstation_ingest, CogyntWorkstationIngest.Broadway.EventPipeline,
   processor_stages: System.get_env("EVENTPIPELINE_PROCESSOR_STAGES") || 22,
   processor_max_demand: System.get_env("EVENTPIPELINE_PROCESSOR_MAX_DEMAND") || 10_000,
@@ -112,89 +113,6 @@ config :cogynt_workstation_ingest, CogyntWorkstationIngest.Servers.Caches.Drilld
 config :cogynt_workstation_ingest, :rpc,
   cogynt_domain: System.get_env("COGYNT_DOMAIN") || "http://localhost",
   cogynt_port: System.get_env("COGYNT_PORT") || 4010
-
-# Kafka Configurations
-config :kafka_ex,
-  # Dev Kafka
-  brokers: [
-    {
-      System.get_env("KAFKA_BROKER") || "172.16.1.100",
-      System.get_env("KAFKA_PORT") || 9092
-    }
-  ],
-  # Local Kafka
-  # brokers: [{"127.0.0.1", 9092}],
-  auto_offset_reset: :earliest,
-  kafka_version: "2.0",
-  commit_interval: System.get_env("KAFKA_COMMIT_INTERVAL") || 1000,
-  commit_threshold: System.get_env("KAFKA_COMMIT_THRESHOLD") || 1000,
-  heartbeat_interval: System.get_env("KAFKA_HEARTBEAT_INTERVAL") || 1000,
-  kafka_client: System.get_env("KAFKA_CLIENT") || KafkaEx,
-  audit_topic: System.get_env("AUDIT_LOG_TOPIC") || "cogynt_audit_log",
-  template_solution_topic: System.get_env("TEMPLATE_SOLUTION_TOPIC") || "template_solutions",
-  template_solution_event_topic:
-    System.get_env("TEMPLATE_SOLUTION_EVENT_TOPIC") || "template_solution_events",
-  topic_partitions: System.get_env("TOPIC_PARTITIONS") || 1,
-  topic_replication: System.get_env("TOPIC_REPLICATION") || 1,
-  topic_config: System.get_env("TOPIC_CONFIG") || []
-
-# Elasticsearch configurations
-config :elasticsearch, :config,
-  enabled: System.get_env("ELASTICSEARCH_ENABLED") || true,
-  basic_authentication_enabled: true,
-  host: System.get_env("ELASTIC_URL") || "http://localhost:9200",
-  username: System.get_env("ELASTIC_USERNAME") || "elasticsearch",
-  password: System.get_env("ELASTIC_PASSWORD") || "elasticsearch",
-  elasticsearch_client: System.get_env("ELASTIC_CLIENT") || Elasticsearch,
-  utc_offset: 0
-
-config :cogynt_workstation_ingest, CogyntWorkstationIngest.Elasticsearch.EventDocument,
-  index_alias: System.get_env("EVENT_INDEX_ALIAS") || "event"
-
-# Cogynt Core key configurations
-config :cogynt_workstation_ingest, :core_keys,
-  crud: System.get_env("CORE_KEYS_CRUD") || "$crud",
-  risk_score: System.get_env("CORE_KEYS_RISK_SCORE") || "_confidence",
-  partial: System.get_env("CORE_KEYS_PARTIAL") || "$partial",
-  events: System.get_env("CORE_KEYS_EVENTS") || "$$events",
-  description: System.get_env("CORE_KEYS_DESCRIPTION") || "$description",
-  entities: System.get_env("CORE_KEYS_ENTITIES") || "$$entities",
-  link_data_type: System.get_env("CORE_KEYS_LINK_DATA_TYPE") || "linkage",
-  update: System.get_env("CORE_KEYS_UPDATE") || "update",
-  delete: System.get_env("CORE_KEYS_DELETE") || "delete",
-  create: System.get_env("CORE_KEYS_CREATE") || "create"
-
-# Broadway Pipelines configurations
-config :cogynt_workstation_ingest, CogyntWorkstationIngest.Broadway.EventPipeline,
-  processor_stages: System.get_env("EVENTPIPELINE_PROCESSOR_STAGES") || 22,
-  processor_max_demand: System.get_env("EVENTPIPELINE_PROCESSOR_MAX_DEMAND") || 10_000,
-  processor_min_demand: System.get_env("EVENTPIPELINE_PROCESSOR_MIN_DEMAND") || 5_000
-
-config :cogynt_workstation_ingest, CogyntWorkstationIngest.Broadway.LinkEventPipeline,
-  processor_stages: System.get_env("LINKEVENTPIPELINE_PROCESSOR_STAGES") || 10,
-  processor_max_demand: System.get_env("LINKEVENTPIPELINE_PROCESSOR_MAX_DEMAND") || 10_000,
-  processor_min_demand: System.get_env("LINKEVENTPIPELINE_PROCESSOR_MIN_DEMAND") || 5_000
-
-config :cogynt_workstation_ingest, CogyntWorkstationIngest.Broadway.DrilldownPipeline,
-  processor_stages: System.get_env("DRILLDOWNPIPELINE_PROCESSOR_STAGES") || 3,
-  processor_max_demand: System.get_env("DRILLDOWNPIPELINE_PROCESSOR_MAX_DEMAND") || 100,
-  processor_min_demand: System.get_env("DRILLDOWNPIPELINE_PROCESSOR_MIN_DEMAND") || 10
-
-config :cogynt_workstation_ingest, CogyntWorkstationIngest.Broadway.Producer,
-  max_retry: System.get_env("PRODUCER_MAX_RETRY") || 1_400,
-  time_delay: System.get_env("PRODUCER_TIME_DELAY") || 60_000
-
-config :cogynt_workstation_ingest, CogyntWorkstationIngest.Broadway.DrilldownProducer,
-  max_retry: System.get_env("DRILLDOWNPIPELINE_PRODUCER_MAX_RETRY") || 1400,
-  time_delay: System.get_env("DRILLDOWNPIPELINE_PRODUCER_TIME_DELAY") || 60000
-
-# Server configurations
-config :cogynt_workstation_ingest, CogyntWorkstationIngest.Servers.Caches.ConsumerRetryCache,
-  time_delay: System.get_env("CONSUMER_RETRY_CACHE_TIME_DELAY") || 600_000,
-  max_retry: System.get_env("CONSUMER_RETRY_CACHE_MAX_RETRY") || 144
-
-config :cogynt_workstation_ingest, CogyntWorkstationIngest.Servers.Caches.DrilldownCache,
-  time_delay: System.get_env("DRILLDOWN_CACHE_TIME_DELAY") || 1_000
 
 # startup utils configurations
 config :cogynt_workstation_ingest, :startup, init_delay: System.get_env("INIT_DELAY") || 5000
