bd97afc74a0731cf41a43ec7efdbc3939538273c
amacciola <amacciola@cogility.com>
2020-03-20 14:35:05 -0700

    Move rpc configs out of constants
    do not use constants for rpc configs

--- lib/cogynt_workstation_ingest_web/rpc/cogynt_client.ex
+++ lib/cogynt_workstation_ingest_web/rpc/cogynt_client.ex
@@ -1,12 +1,10 @@
 defmodule CogyntWorkstationIngestWeb.Rpc.CogyntClient do
   alias JSONRPC2.Clients.HTTP
 
-  @domain Application.get_env(:cogynt_workstation_ingest, :rpc)[:cogynt_domain]
-  @port Application.get_env(:cogynt_workstation_ingest, :rpc)[:cogynt_port]
-  @path "/rpc/ingest"
+  @path "/rpc/cogynt"
 
   def publish_deleted_notifications(notifications) when is_list(notifications) do
-    url = "#{@domain}:#{@port}#{@path}"
+    url = "#{service_name()}:#{service_port()}#{@path}"
     response = HTTP.call(url, "publish:deleted_notifications", notifications)
 
     case response do
@@ -22,7 +20,7 @@ defmodule CogyntWorkstationIngestWeb.Rpc.CogyntClient do
   end
 
   def publish_subscriptions(notifications) when is_list(notifications) do
-    url = "#{@domain}:#{@port}#{@path}"
+    url = "#{service_name()}:#{service_port()}#{@path}"
     response = HTTP.call(url, "publish:subscriptions", notifications)
 
     case response do
@@ -38,7 +36,7 @@ defmodule CogyntWorkstationIngestWeb.Rpc.CogyntClient do
   end
 
   def publish_event_definition_ids(event_definition_ids) when is_list(event_definition_ids) do
-    url = "#{@domain}:#{@port}#{@path}"
+    url = "#{service_name()}:#{service_port()}#{@path}"
     response = HTTP.call(url, "publish:event_definition_ids", event_definition_ids)
 
     case response do
@@ -52,4 +50,11 @@ defmodule CogyntWorkstationIngestWeb.Rpc.CogyntClient do
         {:error, :internal_server_error}
     end
   end
+
+  # ---------------------- #
+  # --- configurations --- #
+  # ---------------------- #
+  defp config(), do: Application.get_env(:cogynt_workstation_ingest, :rpc)
+  defp service_name(), do: config()[:cogynt_otp_service_name]
+  defp service_port(), do: config()[:cogynt_otp_service_port]
 end
