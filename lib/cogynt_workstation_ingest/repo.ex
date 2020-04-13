ed790415a4f555cd70820768dfdd96f4a11d255c
amacciola <amacciola@cogility.com>
2020-04-12 17:24:58 -0700

    Adding back Backfill notifications chagnes
    removing use of Stream in backfill task

--- lib/cogynt_workstation_ingest/repo.ex
+++ lib/cogynt_workstation_ingest/repo.ex
@@ -2,12 +2,4 @@ defmodule CogyntWorkstationIngest.Repo do
   use Ecto.Repo,
     otp_app: :cogynt_workstation_ingest,
     adapter: Ecto.Adapters.Postgres
-
-  def stream_preload(stream, size, preloads) do
-    stream
-    |> Stream.chunk_every(size)
-    |> Stream.flat_map(fn chunk ->
-      CogyntWorkstationIngest.Repo.preload(chunk, preloads)
-    end)
-  end
 end
