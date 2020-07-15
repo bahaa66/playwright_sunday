a1c7afdd603259d7f8b5ec59373c5359d176e4f9
amacciola <amacciola@cogility.com>
2020-07-15 12:11:05 -0700

    updating models and migrations versions

--- mix.exs
+++ mix.exs
@@ -62,18 +62,16 @@ defmodule CogyntWorkstationIngest.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {
         :models,
-        # tag: "v1.5.8",
+        tag: "v1.6.4-beta",
         git: "git@github.com:cogility/cogynt-common.git",
         sparse: "models",
-        branch: "feature/CDST-605-deployment-migrations",
         override: true
       },
       {
         :migrations,
-        # tag: "v1.5.7",
+        tag: "v1.6.4-beta",
         git: "git@github.com:cogility/cogynt-common.git",
         sparse: "migrations",
-        branch: "feature/CDST-605-deployment-migrations",
         override: true
       },
       {:utils,
