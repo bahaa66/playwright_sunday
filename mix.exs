0828ddeff92ea62db31c4f641ddff3650b80d9a0
amacciola <amacciola@cogility.com>
2020-11-04 10:22:19 -0800

    fix(elastic): update version

--- mix.exs
+++ mix.exs
@@ -85,7 +85,7 @@ defmodule CogyntWorkstationIngest.MixProject do
       },
       {
         :elasticsearch,
-        tag: "v1.10.4-beta",
+        tag: "v1.10.5-beta",
         git: "git@github.com:cogility/cogynt-common.git",
         sparse: "elasticsearch",
         override: true
