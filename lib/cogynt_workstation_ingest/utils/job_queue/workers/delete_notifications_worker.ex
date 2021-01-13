2f245f9008c368ad1a848fb05485e0f4f825c230
amacciola <amacciola@cogility.com>
2021-01-13 11:58:39 -0800

    fix(notifiation_tasks): prev_status fix

--- lib/cogynt_workstation_ingest/utils/job_queue/workers/delete_notifications_worker.ex
+++ lib/cogynt_workstation_ingest/utils/job_queue/workers/delete_notifications_worker.ex
@@ -87,7 +87,7 @@ defmodule CogyntWorkstationIngest.Utils.JobQueue.Workers.DeleteNotificationsWork
             event_definition.id,
             topic: event_definition.topic,
             status: consumer_state.prev_status,
-            prev_status: consumer_state.status
+            prev_status: prev_status: ConsumerStatusTypeEnum.status()[:running]
           )
 
           if consumer_state.prev_status == ConsumerStatusTypeEnum.status()[:running] do
