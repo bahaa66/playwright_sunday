fabcd4fd53cee30f0e325a1572f3fea21f161427
amacciola <amacciola@cogility.com>
2021-04-05 14:17:41 -0700

    add back changes

--- lib/cogynt_workstation_ingest/broadway/event_processor.ex
+++ lib/cogynt_workstation_ingest/broadway/event_processor.ex
@@ -549,20 +549,7 @@ defmodule CogyntWorkstationIngest.Broadway.EventProcessor do
                   if v2 == @defaults.risk_history_document or Enum.empty?(v2) do
                     v1
                   else
-                    if Enum.empty?(v1) do
-                      v1 ++ [v2]
-                    else
-                      temp_doc = List.first(v1)
-                      new_risk = temp_doc.risk_history ++ v2.risk_history
-
-                      [
-                        %{
-                          id: v2.id,
-                          event_definition_id: v2.event_definition_id,
-                          risk_history: new_risk
-                        }
-                      ]
-                    end
+                    v1 ++ [v2]
                   end
 
                 _ ->
