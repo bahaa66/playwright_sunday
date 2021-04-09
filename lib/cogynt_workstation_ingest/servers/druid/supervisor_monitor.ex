bc9a19de9d057cec92f53c6c0e0d6beaf33100fd
cpelletier-cogility <cpelletier@cogility.com>
2021-04-09 16:27:30 -0700

    Added functions for getting status info

--- lib/cogynt_workstation_ingest/servers/druid/supervisor_monitor.ex
+++ lib/cogynt_workstation_ingest/servers/druid/supervisor_monitor.ex
@@ -19,11 +19,25 @@ defmodule CogyntWorkstationIngest.Servers.Druid.SupervisorMonitor do
     quote do
       use GenServer
 
+      @status_check_interval 30_000
+
       # -------------------- #
       # --- client calls --- #
       # -------------------- #
       def start_link do
         GenServer.start_link(__MODULE__, [], name: __MODULE__)
+      end
+
+      def supervisor_status do
+        GenServer.call(__MODULE__, :supervisor_status)
+      end
+
+      def healthy? do
+        GenServer.call(__MODULE__, :healthy?)
+      end
+
+      def state do
+        GenServer.call(__MODULE__, :state)
       end
 
       # ------------------------ #
@@ -34,8 +48,8 @@ defmodule CogyntWorkstationIngest.Servers.Druid.SupervisorMonitor do
         Druid.status_health()
         |> case do
           {:ok, true} ->
-            {:ok, %{supervisor_status: %{"state" => "LOADING"}},
-             {:continue, {:create_or_update_supervisor, unquote(supervisor_id)}}}
+            {:ok, %{id: unquote(supervisor_id), supervisor_status: %{"state" => "LOADING"}},
+             {:continue, :create_or_update_supervisor}}
 
           {:ok, false} ->
             CogyntLogger.error(
@@ -56,19 +70,53 @@ defmodule CogyntWorkstationIngest.Servers.Druid.SupervisorMonitor do
       end
 
       @impl true
-      def handle_continue({:create_or_update_supervisor, id}, state) do
+      def handle_call(:supervisor_status, _from, %{supervisor_status: status} = state) do
+        {:reply, status, state}
+      end
+
+      @impl true
+      def handle_call(:healthy?, _from, %{supervisor_status: status} = state) do
+        {:reply, Map.get(status, "healthy", false), state}
+      end
+
+      @impl true
+      def handle_call(:state, _from, %{supervisor_status: status} = state) do
+        {:reply, Map.get(status, "state", "UNKNOWN"), state}
+      end
+
+      @impl true
+      def handle_continue(:create_or_update_supervisor, %{id: id} = state) do
         brokers = unquote(brokers)
         dimensions = unquote(dimensions)
         supervisor_spec = Druid.Utils.base_kafka_supervisor(id, brokers, dimensions: dimensions)
 
         with {:ok, %{"id" => id}} <- Druid.create_or_update_supervisor(supervisor_spec),
              {:ok, %{"payload" => payload}} <- Druid.get_supervisor_status(id) do
+          Process.send_after(__MODULE__, :update_status, @status_check_interval)
           {:noreply, %{state | supervisor_status: payload}}
         else
           {:error, error} ->
             CogyntLogger.error(
               "#{__MODULE__}",
-              "Unable to create and get supervisor information: #{inspect(error)}"
+              "Unable to create and get supervisor information for #{id}: #{inspect(error)}"
+            )
+
+            {:noreply, %{state | supervisor_status: {:error, error}}}
+        end
+      end
+
+      @impl true
+      def handle_info(:update_status, %{id: id} = state) do
+        Druid.get_supervisor_status(id)
+        |> case do
+          {:ok, %{"payload" => payload} = s} ->
+            Process.send_after(__MODULE__, :update_status, @status_check_interval)
+            {:noreply, %{state | supervisor_status: payload}}
+
+          {:error, error} ->
+            CogyntLogger.error(
+              "#{__MODULE__}",
+              "Unable to get Druid supervisor status for #{id}: #{inspect(error)}"
             )
 
             {:noreply, %{state | supervisor_status: {:error, error}}}
