b870e0253228dae25e0337afae593b177e4bee3c
cpelletier-cogility <cpelletier@cogility.com>
2021-05-14 16:41:33 -0700

    Start of support for avro

--- lib/cogynt_workstation_ingest/servers/druid/template_solution_events.ex
+++ lib/cogynt_workstation_ingest/servers/druid/template_solution_events.ex
@@ -3,6 +3,7 @@ defmodule CogyntWorkstationIngest.Servers.Druid.TemplateSolutionEvents do
 
   use CogyntWorkstationIngest.Servers.Druid.SupervisorMonitor,
     supervisor_id: Config.template_solution_events_topic(),
+    use_avro: true,
     brokers:
       Config.kafka_brokers()
       |> Enum.map(fn {host, port} -> "#{host}:#{port}" end)
@@ -34,5 +35,42 @@ defmodule CogyntWorkstationIngest.Servers.Druid.TemplateSolutionEvents do
           }
         ]
       }
+    },
+    avro_schema: %{
+      fields: [
+        %{
+          name: "templateTypeName",
+          type: "string"
+        },
+        %{
+          name: "templateTypeId",
+          type: "string"
+        },
+        %{
+          name: "id",
+          type: "string"
+        },
+        %{
+          name: "aid",
+          type: [
+            "null",
+            "string"
+          ]
+        },
+        %{
+          name: "assertionName",
+          type: [
+            "null",
+            "string"
+          ]
+        },
+        %{
+          name: "event",
+          type: "string"
+        }
+      ],
+      name: "SolutionEvent",
+      namespace: "com.cogility.hcep.generated",
+      type: "record"
     }
 end
--- lib/cogynt_workstation_ingest/servers/druid/template_solutions.ex
+++ lib/cogynt_workstation_ingest/servers/druid/template_solutions.ex
@@ -3,6 +3,7 @@ defmodule CogyntWorkstationIngest.Servers.Druid.TemplateSolutions do
 
   use CogyntWorkstationIngest.Servers.Druid.SupervisorMonitor,
     supervisor_id: Config.template_solutions_topic(),
+    use_avro: true,
     brokers:
       Config.kafka_brokers()
       |> Enum.map(fn {host, port} -> "#{host}:#{port}" end)
@@ -12,8 +13,31 @@ defmodule CogyntWorkstationIngest.Servers.Druid.TemplateSolutions do
         "id",
         "template_type_name",
         "template_type_id",
-        "events",
-        "outcomes"
+        "retracted"
+      ]
+    },
+    # TODO: Change this to use the schema registry url and use "schema_repo" type
+    avro_schema: %{
+      name: "TemplateSolution",
+      namespace: "com.cogility.hcep.generated",
+      type: "record",
+      fields: [
+        %{
+          name: "templateTypeName",
+          type: "string"
+        },
+        %{
+          name: "templateTypeId",
+          type: "string"
+        },
+        %{
+          name: "id",
+          type: "string"
+        },
+        %{
+          name: "retracted",
+          type: "boolean"
+        }
       ]
     }
 end
