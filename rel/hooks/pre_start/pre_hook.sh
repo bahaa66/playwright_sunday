e070a40845ddbe0538b5ee41065f1d12a7927514
langleyl <llangley@cogility.com>
2021-09-29 10:44:56 -0700

    fix(build): libcluster rel config

--- rel/hooks/pre_start/pre_hook.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo 'Setting ERLANG_NAME...'
-export ERLANG_NAME=$POD_IP
-echo $ERLANG_NAME
-export ERLANG_COOKIE=">CJ!Fd]3C?7RL%&F1%{F=6@;{i&HX4{XjhZ3vJCrV;9tg3bO|6:n5Oi9BtnI,A(h"
-echo $ERLANG_COOKIE
