a0e017d9fe1d150cd2ba98bc2b01cbe7389a8840
langleyl <llangley@cogility.com>
2021-09-29 10:13:08 -0700

    fix(build): libcluster cookie typo

--- rel/hooks/pre_start/pre_hook.sh
+++ rel/hooks/pre_start/pre_hook.sh
@@ -3,5 +3,5 @@
 echo 'Setting ERLANG_NAME...'
 export ERLANG_NAME=$POD_IP
 echo $ERLANG_NAME
-export ERLANG_COOKIE">CJ!Fd]3C?7RL%&F1%{F=6@;{i&HX4{XjhZ3vJCrV;9tg3bO|6:n5Oi9BtnI,A(h"
+export ERLANG_COOKIE=">CJ!Fd]3C?7RL%&F1%{F=6@;{i&HX4{XjhZ3vJCrV;9tg3bO|6:n5Oi9BtnI,A(h"
 echo $ERLANG_COOKIE
