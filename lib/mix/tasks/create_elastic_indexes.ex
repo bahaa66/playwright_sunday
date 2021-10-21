74fe31b14aae77affa34ea96fa355183f954f156
tahseen447 <tahseenfatima1986@gmail.com>
2021-10-21 16:05:03 -0700

    update mix task to start ecto and postgrex before checking elastic indexes

--- lib/mix/tasks/create_elastic_indexes.ex
+++ lib/mix/tasks/create_elastic_indexes.ex
@@ -10,34 +10,35 @@ defmodule Mix.Tasks.CreateElasticIndexes do
 
   @impl Mix.Task
   def run(_) do
-    with {:ok, _} <- HTTPoison.start(),
-        {:ok, _fun_return, _apps } <- Ecto.Migrator.with_repo(CogyntWorkstationIngest.Repo, &Ecto.Migrator.run(&1, :up, all: true)),
-        {:ok, _} <- CogyntWorkstationIngest.Repo.start_link(),
-        {:ok, _} <- CogyntWorkstationIngest.Elasticsearch.Cluster.start_link(),
-        {:ok, false} <- ElasticsearchAPI.index_exists?(Config.event_index_alias()),
-         {:ok, _ } <- ElasticsearchAPI.create_index(Config.event_index_alias()) do
-      Mix.shell().info("The index: #{Config.event_index_alias()} for Cogynt has been created.")
-    else
-      {:ok, true} ->
-        ElasticsearchAPI.check_to_reindex()
-        #TBD: should you stop the services that were started?
-        # [:connection, :db_connection, :decimal, :ecto, :ecto_sql, :postgrex]
-        Mix.shell().info("The index: #{Config.event_index_alias()} already exists.")
+       with {:ok, _} <- HTTPoison.start(),
+       {:ok, _} <- Application.ensure_all_started(:ecto),
+       {:ok, _} <- Application.ensure_all_started(:postgrex),
+       {:ok, _} <- CogyntWorkstationIngest.Repo.start_link(),
+       {:ok, _} <- CogyntWorkstationIngest.Elasticsearch.Cluster.start_link(),
+       {:ok, false} <- ElasticsearchAPI.index_exists?(Config.event_index_alias()),
+       {:ok, _ } <- ElasticsearchAPI.create_index(Config.event_index_alias()) do
+        Mix.shell().info("The index: #{Config.event_index_alias()} for Cogynt has been created.")
+      else
+        {:ok, true} ->
+          ElasticsearchAPI.check_to_reindex()
+          #TBD: should you stop the services that were started?
+          # [:connection, :db_connection, :decimal, :ecto, :postgrex]
+          Mix.shell().info("The index: #{Config.event_index_alias()} already exists.")
 
-      {:error, _} ->
-        CogyntLogger.error(
-          "#{__MODULE__}",
-          "An error occured trying to create the index #{Config.event_index_alias()}"
-        )
+        {:error, _} ->
+          CogyntLogger.error(
+            "#{__MODULE__}",
+            "An error occured trying to create the index #{Config.event_index_alias()}"
+          )
 
-        Mix.raise("""
-          An error occured trying to create the index #{Config.event_index_alias()}
-        """)
+          Mix.raise("""
+            An error occured trying to create the index #{Config.event_index_alias()}
+          """)
 
-      _ ->
-        Mix.raise("""
-          An unexpected error occurred trying to create the indexes.
-        """)
-    end
-  end
+        _ ->
+          Mix.raise("""
+            An unexpected error occurred trying to create the indexes.
+          """)
+      end
+      end
 end
