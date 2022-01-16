cc108c979d9879048368c4c6dceb6cd0f83dd9c1
sgraham785 <sgraham785@gmail.com>
2022-01-15 21:37:37 -0800

    fix: configured libcluster for k8s DNSSRV

--- config/dev.exs
+++ config/dev.exs
@@ -168,13 +168,11 @@ config :druid,
 config :libcluster,
   topologies: [
     k8s_ws_ingest: [
-      strategy: Elixir.Cluster.Strategy.Kubernetes,
+      strategy: Elixir.Cluster.Strategy.Kubernetes.DNSSRV,
       config: [
-        mode: :hostname,
-        kubernetes_service_name: "ws-ingest-otp",
-        kubernetes_node_basename: "ws-ingest-otp",
-        kubernetes_selector: "k8s.cogynt.io/name=ws-ingest-otp",
-        kubernetes_namespace: System.get_env("NAMESPACE") || "cogynt-kots",
+        namespace: System.get_env("NAMESPACE") || "cogynt-kots",
+        service: System.get_env("SERVICE_NAME") || "ws-ingest-otp-headless",
+        application_name: "ws-ingest-otp",
         polling_interval: 10_000
       ]
     ]
