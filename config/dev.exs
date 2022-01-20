078d43b3ecd1b33d5b55add54c32e1692f177977
amacciola <amacciola@cogility.com>
2022-01-20 09:59:03 -0600

    fix(debugging): libcluster

--- config/dev.exs
+++ config/dev.exs
@@ -170,11 +170,9 @@ config :libcluster,
     k8s_ws_ingest: [
       strategy: Elixir.Cluster.Strategy.Kubernetes,
       config: [
-        mode: :dns,
-        namespace: System.get_env("NAMESPACE") || "cogynt-kots",
-        service: "ws-ingest-otp-headless",
+        mode: :hostname,
+        kubernetes_service_name: "ws-ingest-otp-headless",
         # service: System.get_env("SERVICE_NAME") || "ws-ingest-otp-headless",
-        application_name: "ws-ingest-otp",
         kubernetes_node_basename: "ws-ingest-otp",
         kubernetes_selector: "k8s.cogynt.io/name=ws-ingest-otp",
         kubernetes_namespace: System.get_env("NAMESPACE") || "cogynt-kots",
