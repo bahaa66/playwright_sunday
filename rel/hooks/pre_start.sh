0e9c2ea9c36e6ae64a58d90d1707c346afbf68f5
amacciola <amacciola@cogility.com>
2022-02-01 17:59:02 -0600

    fix(startup): add elasticsearch to startup task

--- rel/hooks/pre_start.sh
+++ rel/hooks/pre_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-echo "Running Elasticsearch startup tasks"
-server eval "Elixir.CogyntWorkstationIngest.ReleaseTasks.eval_elasticsearch"
-echo "Elasticsearch Event Index ran succesfully"
+echo "Nothing to run for pre_start...."
+#server eval "Elixir.CogyntWorkstationIngest.ReleaseTasks.eval_elasticsearch"
+#echo "Elasticsearch Event Index ran succesfully"
