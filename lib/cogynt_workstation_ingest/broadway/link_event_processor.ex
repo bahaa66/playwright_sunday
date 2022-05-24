1f81712d531cae7424bfef8c5cb7bebe5239890a
amacciola <amacciola@cogility.com>
2022-05-24 17:06:05 -0500

    debug logs

--- lib/cogynt_workstation_ingest/broadway/link_event_processor.ex
+++ lib/cogynt_workstation_ingest/broadway/link_event_processor.ex
@@ -51,6 +51,8 @@ defmodule CogyntWorkstationIngest.Broadway.LinkEventProcessor do
   Itterates through the entities object on the link_event and builds the associations
   """
   def process_entities(%{validated: false} = data) do
+    IO.puts("This log means that: #{data.event_type} is being treated as NON Linkage")
+
     data
     |> Map.put(:elastic_event_links, nil)
     |> Map.put(:pipeline_state, :process_entities)
@@ -119,6 +121,8 @@ defmodule CogyntWorkstationIngest.Broadway.LinkEventProcessor do
             true ->
               []
           end
+
+        IO.inspect(elastic_event_links, label: "elastic_event_links to be inserted into Elastic")
 
         Map.put(data, :pg_event_links, pg_event_links)
         |> Map.put(:pg_event_links_delete, pg_event_links_delete)
