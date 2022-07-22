0a09ecddcfb3f109c0d1024359f1dfc7353c62b7
amacciola <amacciola@cogility.com>
2022-07-22 18:27:03 -0500

    remove new line

--- lib/cogynt_workstation_ingest/events/events_context.ex
+++ lib/cogynt_workstation_ingest/events/events_context.ex
@@ -817,15 +817,13 @@ defmodule CogyntWorkstationIngest.Events.EventsContext do
           bulk_transactional_data.delete_core_id ++ bulk_transactional_data.pg_event_links_delete
         )
 
-      case Repo.query("SELECT event_pipeline_bulk_upsert(
-            '{#{bulk_transactional_data.pg_event_string}}'::events_composite_type[],
-            '{#{bulk_transactional_data.pg_event_links}}'::event_links_composite_type[],
-            '{#{bulk_transactional_data.pg_event_history}}'::event_history_composite_type[],
-            '{#{bulk_transactional_data.pg_notifications}}'::notifications_composite_type[],
+      case Repo.query(
+             "SELECT event_pipeline_bulk_upsert('{#{bulk_transactional_data.pg_event_string}}'::events_composite_type[], '{#{bulk_transactional_data.pg_event_links}}'::event_links_composite_type[], '{#{bulk_transactional_data.pg_event_history}}'::event_history_composite_type[], '{#{bulk_transactional_data.pg_notifications}}'::notifications_composite_type[],
             CAST('#{remove_notification_core_ids}'::UUID[]),
             CAST('#{remove_event_link_core_ids}'::UUID[]),
             CAST('#{bulk_transactional_data.delete_core_id}'::UUID[])
-            )") do
+            )"
+           ) do
         {:ok, result} ->
           {:ok, result}
 
