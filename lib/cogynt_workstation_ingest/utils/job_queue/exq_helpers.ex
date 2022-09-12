f718fcaf44e7fe789de2f9efc8b65daf6c15adc8
amacciola <amacciola@cogility.com>
2022-09-12 08:47:50 -0700

    fix(exq): EXQ.API.processes return object

--- lib/cogynt_workstation_ingest/utils/job_queue/exq_helpers.ex
+++ lib/cogynt_workstation_ingest/utils/job_queue/exq_helpers.ex
@@ -86,21 +86,38 @@ defmodule CogyntWorkstationIngest.Utils.JobQueue.ExqHelpers do
 
   def queue_finished_processing?(queue_prefix, queue_id) do
     try do
-      IO.inspect(queue_prefix, label: "Queue Prefix")
-      IO.inspect(queue_id, label: "Queue ID")
-
       Enum.reduce([queue_prefix], true, fn prefix, acc ->
         queue_name = prefix <> "-" <> "#{queue_id}"
-        IO.inspect(queue_name, label: "Queue Name")
         {:ok, count} = Exq.Api.queue_size(Exq.Api, queue_name)
+        # Exq.Api.processes returns a list of structs that looks like
+        # [
+        #   %Exq.Support.Process{
+        #     host: "cogynt-ws-ingest-otp",
+        #     payload: %Exq.Support.Job{
+        #       args: ["7f3b93aa-bf67-47c3-88d2-c8bc4bf9b0d6"],
+        #       class: "CogyntWorkstationIngest.Utils.JobQueue.Workers.BackfillNotificationsWorker",
+        #       enqueued_at: 1_662_997_299.775067,
+        #       error_class: nil,
+        #       error_message: nil,
+        #       failed_at: nil,
+        #       finished_at: nil,
+        #       jid: "f8c135e6-3b43-476a-8238-5e43720d2c2d",
+        #       processor: nil,
+        #       queue: "notifications-f71ef2d1-7cd3-54d8-87cf-4b8040595698",
+        #       retried_at: nil,
+        #       retry: 1,
+        #       retry_count: nil
+        #     },
+        #     pid: "#PID<0.5838.0>",
+        #     queue: "notifications-f71ef2d1-7cd3-54d8-87cf-4b8040595698",
+        #     run_at: 1_662_997_299.786869
+        #   }
+        # ]
         {:ok, processes} = Exq.Api.processes(Exq.Api)
 
-        IO.inspect(processes, label: "Processes", pretty: true)
-
         grouped =
-          Enum.group_by(processes, fn process ->
-            decoded_job = Jason.decode!(process.job, keys: :atoms)
-            decoded_job.queue
+          Enum.group_by(processes, fn %Exq.Support.Process{queue: q} = process ->
+            q
           end)
 
         queue_processes = Map.get(grouped, queue_name, [])
